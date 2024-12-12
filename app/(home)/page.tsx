e7de9930eb8dbe02e35e68e65a52d6a4cf3d9b6f
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2024-12-11 17:10:45 -0700

    fetch data

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,31 +1,20 @@
-"use client";
-import { useEffect, useState } from "react";
+export const metadata = {
+  title: "Home"
+}
+const URL = "https://billions-api.nomadcoders.workers.dev/"
 
-export default function Home() {
-  const [billionaries, setBillionaries] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const getBillionaries = async () => {
-    const response = await fetch(
-      "https://billions-api.nomadcoders.workers.dev/"
-    );
-    const json = await response.json();
-    setBillionaries(json);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getBillionaries();
-  }, []);
-
+async function getBillionaries() {
+  const response =  await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+export default async function HomePage() {
+  const billionaries = await getBillionaries();
+    
   return (
     <div>
       <h2>home</h2>
-      {isLoading ? (
-        <h2>is Loading....</h2>
-      ) : (
-        <h2>{JSON.stringify(billionaries)}</h2>
-      )}
+      {JSON.stringify(billionaries)}
       <div></div>
     </div>
   );
